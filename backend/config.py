4cf032177f5dc658ce565bec7f2493ab71114d12
Yash Modi <yashm28@Yashs-MacBook-Pro.local>
2026-01-10 14:51:14 -0800

    bugfix

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ class Settings:
     """Application settings and configuration"""
     
     # MongoDB settings
-    MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb+srv://mongo-hack:Ambrosi%40@cluster0.uibdjo.mongodb.net")
+    MONGODB_URL: str = os.getenv("MONGODB_URL", "<URI>")
     MONGODB_DB_NAME: str = os.getenv("MONGODB_DB_NAME", "tether")
 
 settings = Settings()
